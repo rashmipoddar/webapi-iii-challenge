ad11a167ce6e3dfed974b4c4012819de4a419d2a
Rashmi <poddar.rashmi@gmail.com>
2019-10-31 11:32:09 -0400

    Added endpoint to create a new user and used custom middleware to check new user data

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = 'express';
+const express = require('express');
+
+const userRoutes = require('./users/userRouter');
+const postRoutes = require('./posts/postRouter');
 
 const server = express();
+
+server.use(express.json());
+server.use('/api/users', userRoutes);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,20 @@
-const express = 'express';
+const express = require('express');
+
+const db = require('./userDb');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post('/', validateUser, (req, res) => {
+  const newUser = req.body;
+  db.insert(newUser)
+    .then(response => {
+      console.log(response);
+      res.status(201).send(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send({message: 'There was an error in creating the user. Please try again.'})
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -11,10 +22,36 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
+  db.get()
+    .then(response => {
+      // console.log(response);
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({message: 'There was an error in getting users from the database'});
+    })
 
 });
 
 router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+
+  db.getById(id)
+    .then(response => {
+      console.log(response);
+      if (response) {
+        res.status(200).send(response);
+      } else {
+        res.status(404).send({message: 'User with the given id does not exist'});
+      }
+      
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send({message: 'There was an error in getting users from the database'});
+    })
 
 });
 
@@ -37,7 +74,13 @@ function validateUserId(req, res, next) {
 };
 
 function validateUser(req, res, next) {
-
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).send({message: 'Missing user data'});
+  } else if (!req.body.name) {
+    res.status(400).send({message: 'Missing required name field'});
+  } else {
+    next()
+  }
 };
 
 function validatePost(req, res, next) {
