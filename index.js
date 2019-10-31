2dfecef83c88f1322c8fabaeb8f57146a7ce861f
Rashmi <poddar.rashmi@gmail.com>
2019-10-31 19:02:22 -0400

    Added dotenv package and set start script

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // code away!
 
 const server = require('./server');
+require('dotenv').config()
 
-server.listen(5000, () => 
+const port = process.env.PORT;
+
+server.listen(port, () => 
   console.log('Server running on port 5000')
 )
